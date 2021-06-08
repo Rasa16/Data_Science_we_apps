f0b88375c31acf816b424edfbbf8e1b833cb0854
Rahul Sahni <55609204+Rasa16@users.noreply.github.com>
2021-06-08 20:30:32 +0530

    Add files via upload

---
+++ Stock_price_web_app/myapp.py
@@ -0,0 +1,30 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of Google!
+
+* RAHUL
+	1. Python
+	2. CPP
+
+[LinkedIn](https://www.linkedin.com/in/rahul-sahni-/)
+
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'MSFT'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.write("""
+	## Closing price
+	""")
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
